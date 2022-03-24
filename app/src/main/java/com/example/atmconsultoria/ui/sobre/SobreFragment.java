db88c1fc8b20d8f447923f0e0e23f00ac08923fa
Lindembergh Madruga <lindemberghferreira@gmail.com>
2022-03-24 19:24:31 -0300

    Concluindo o aplicativo

--- app/src/main/java/com/example/atmconsultoria/ui/sobre/SobreFragment.java
+++ app/src/main/java/com/example/atmconsultoria/ui/sobre/SobreFragment.java
@@ -14,13 +14,16 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.atmconsultoria.R;
 
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
 public class SobreFragment extends Fragment {
 
     private SobreViewModel toolsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        toolsViewModel =
+       /* toolsViewModel =
                 ViewModelProviders.of(this).get(SobreViewModel.class);
         View root = inflater.inflate(R.layout.fragment_sobre, container, false);
         final TextView textView = root.findViewById(R.id.text_sobre);
@@ -30,6 +33,24 @@ public class SobreFragment extends Fragment {
                 textView.setText(s);
             }
         });
-        return root;
+        return root;*/
+        Element versao = new Element();
+        versao.setTitle("Versão 1.0");
+
+        String descricao = "A ATM Consultoria tem como missão apoiar organizações que desejam alcançar o sucesso através da excelência em gestão e da busca pela qualidade";
+        return new AboutPage(getActivity()).setImage(R.drawable.logo)
+                .setDescription(descricao)
+                .addGroup("Entre em contato")
+                .addEmail("atendimento@consultoria.com.br", "Envie um email")
+                .addWebsite("https://www.google.com/","Acesse nosso site").addGroup("Redes sociais")
+                .addFacebook("limadruga", "Facebook")
+                .addInstagram("limadruga", "Instagram")
+                .addTwitter("limadruga", "Twitter")
+                .addGitHub("limadruga", "GitHub")
+                .addPlayStore("com.facebook.katana", "Download App")
+                .addItem(versao)
+                .create();
+
+
     }
 }
